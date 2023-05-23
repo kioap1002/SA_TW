60eed7ef27d9738d832c0bc5540cc1dafa6ce005
dreamer5227 <B11023005@gemail.yuntech.edu.tw>
2023-05-24 00:35:54 +0800

    資料庫修改

--- main/intersectionsDB.java
+++ main/intersectionsDB.java
@@ -5,35 +5,56 @@ import java.util.stream.Collectors;
 import java.util.ArrayList;
 
 public class intersectionsDB {  //一直更新，單位為秒
-    private double density;
+    private int count = 0;
     List <roadSituation> intersectionData_EW = new Arraylist<roadSituation>();
     List <roadSituation> intersectionData_NS = new Arraylist<roadSituation>();
     public void addIntersectionData(roadSituation RS_EW,roadSituation RS_NS){
         intersectionData_EW.add(RS_EW);
         intersectionData_NS.add(RS_NS);
+        count++;
     }
     //計算今日平均密度
     public double calculateTodayDensityAverage(boolean laneDirection){  
         double TodaysDensityAverage;
-        double count=0.0;
-        for(roadSituation rS : intersectionData_EW){
-            TodaysDensityAverage += rS.density;
-            count += 1.0;
+        if(laneDirection){
+            for(roadSituation rS : intersectionData_NS){
+                TodaysDensityAverage += rS.density;
+            }
+        } else {
+            for(roadSituation rS : intersectionData_EW){
+                TodaysDensityAverage += rS.density;
+            }
         }
-        TodaysDensityAverage /=count; 
+        TodaysDensityAverage /= (double)count; 
         return TodaysDensityAverage;
     }
     //計算今日平均車數
     public double calculateTodayVehicleAmountAverage(boolean laneDirection){  
         double vehicleAmountAverage;
-        
+        if(laneDirection){
+            for(roadSituation rS : intersectionData_NS){
+                vehicleAmountAverage += (double)rS.vehicleAmount;
+            }
+        } else {
+            for(roadSituation rS : intersectionData_EW){
+                vehicleAmountAverage += (double)rS.vehicleAmount;
+            }
+        }
+        vehicleAmountAverage /= (double)count;
         return vehicleAmountAverage;
     }
     //計算今日緊急次數
     public double calculateTodayEmergencyVehicleCount(boolean laneDirection){  
-        double emergencyVehicleCount;
-        
+        int emergencyVehicleCount;
+        if(laneDirection){
+            for(roadSituation rS : intersectionData_NS){
+                if(rS.emergencyVehicle) emergencyVehicleCount++;
+            }
+        } else {
+            for(roadSituation rS : intersectionData_EW){
+                if(rS.emergencyVehicle) emergencyVehicleCount++;
+            }
+        }
         return emergencyVehicleCount;
     }
-
 }
