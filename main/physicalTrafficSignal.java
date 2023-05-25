8cfa0679d3b562c9aadd344050b8c9d2ab5e7d08
dreamer5227 <B11023005@gemail.yuntech.edu.tw>
2023-05-25 11:43:53 +0800

    Update physicalTrafficSignal.java
    更改實體紅綠燈的紅綠燈模式倒數方法

--- main/physicalTrafficSignal.java
+++ main/physicalTrafficSignal.java
@@ -1,7 +1,7 @@
 package main;
 
 public class physicalTrafficSignal {
-    int[] now_Light = {0, 0};//0: 紅, 1: 綠, 2: 黃
+    int[] now_Light = {0, 0, 0};//0: 紅, 1: 綠, 2: 黃，第三個代表現在輪到哪一方，0: 閃燈 1: EW 2: NS
     int[] EW_Light = {0, 0, 0}; //綠, 黃, 紅 //綠: 0, 1 黃: 0, 1, 2 紅: 0, 1, 2 //0: 沒亮 1: 有亮 2: 閃燈
     int[] NS_Light = {0, 0, 0}; //綠, 黃, 紅
     // physicalTrafficSignal(){
@@ -9,96 +9,68 @@ public class physicalTrafficSignal {
     // }
     int seconds = 0; // 倒數秒數
     public void trafficLightTime(double greenLightTime_EW, double yellowLightTime, double greenLightTime_NS, double redLightTime ){
-        // private long secend_pri = System.currentTimeMillis() / 1000;
-        // private long secend_now = System.currentTimeMillis() / 1000;
-        // boolean 
         while(true){ //應該會在controller做
             //secend_now = System.currentTimeMillis() / 1000;
-            seconds = (int)greenLightTime_EW; // 倒數秒數
             //切換成東西向綠燈
             EW_side_Passable_g();
-            for (int i = seconds; i >= 0; i--) {
-                try {
-                    Thread.sleep(1000); // 暫停 1 秒
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+            countDown((int)greenLightTime_EW * 1000); // EW綠燈時間
             EW_side_Passable_y();
-            try {
-                Thread.sleep((int)yellowLightTime*1000); // 黃燈時間
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            Both_side_Passable_r();
-            try {
-                Thread.sleep((int)yellowLightTime*1000); // 全紅時間
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            seconds = (int)greenLightTime_NS;
-            for (int i = seconds; i >= 0; i--) {
-                try {
-                    Thread.sleep(1000); // 暫停 1 秒
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            EW_side_Passable_y();
-            try {
-                Thread.sleep((int)yellowLightTime*1000); // 黃燈時間
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            Both_side_Passable_r();
-            try {
-                Thread.sleep((int)yellowLightTime*1000); // 全紅時間
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
+            countDown((int)yellowLightTime * 1000); // 黃燈時間
+            EW_side_Passable_ar();
+            countDown((int)redLightTime * 1000); //全紅時間
+            NS_side_Passable_g();
+            countDown((int)greenLightTime_NS * 1000); // NS綠燈時間
+            NS_side_Passable_y();
+            countDown((int)yellowLightTime * 1000); // 黃燈時間
+            NS_side_Passable_ar();
+            countDown((int)redLightTime * 1000); //全紅時間
         }
     }
     public int[] EW_side_Passable_g (){
         EW_Light = new int[] {1, 0, 0};
         NS_Light = new int[] {0, 0, 1};
-        now_Light = new int[] {1, 0};
+        now_Light = new int[] {1, 0, 1};
         return now_Light;
     }
     public int[] EW_side_Passable_y(){
         EW_Light = new int[] {0, 1, 0};
         NS_Light = new int[] {0, 0, 1};
-        now_Light = new int[] {2, 0};
+        now_Light = new int[] {2, 0, 1};
         return now_Light;
     }
-    public int[] Both_side_Passable_r(){
+    public int[] EW_side_Passable_ar(){
         EW_Light = new int[] {0, 0, 1};
         NS_Light = new int[] {0, 0, 1};
-        now_Light = new int[] {0, 0};
+        now_Light = new int[] {0, 0, 1};
         return now_Light;
     }
     public int[] NS_side_Passable_g(){
         EW_Light = new int[] {0, 0, 1};
         NS_Light = new int[] {1, 0, 0};
-        now_Light = new int[] {0, 1};
+        now_Light = new int[] {0, 1, 2};
         return now_Light;
     }
     public int[] NS_side_Passable_y(){
         EW_Light = new int[] {0, 0, 1};
         NS_Light = new int[] {0, 1, 0};
-        now_Light = new int[] {0, 2};
+        now_Light = new int[] {0, 2, 2};
         return now_Light;
     }
-
+    public int[] NS_side_Passable_ar(){
+        EW_Light = new int[] {0, 0, 1};
+        NS_Light = new int[] {0, 0, 1};
+        now_Light = new int[] {0, 0, 2};
+        return now_Light;
+    }
     public int[] trafficLightFlashing(int right){
         if (right == 1) {
             EW_Light = new int[] {0, 2, 0};
             NS_Light = new int[] {0, 0, 2};
-            now_Light = new int[] {2, 0};
+            now_Light = new int[] {2, 0, 0};
         } else {
             EW_Light = new int[] {0, 0, 2};
             NS_Light = new int[] {0, 2, 0};
-            now_Light = new int[] {0, 2};
+            now_Light = new int[] {0, 2, 0};
         }
         return now_Light;
     }
@@ -106,15 +78,15 @@ public class physicalTrafficSignal {
         if (condition == 1) {
             EW_Light = new int[] {1, 0, 0};
             NS_Light = new int[] {0, 0, 1};
-            now_Light = new int[] {1, 0};
+            now_Light = new int[] {1, 0, 1};
         } else if (condition == 2){
             EW_Light = new int[] {0, 0, 1};
             NS_Light = new int[] {1, 0, 0};
-            now_Light = new int[] {0, 1};
+            now_Light = new int[] {0, 1, 2};
         } else {
             EW_Light = new int[] {0, 0, 1};
             NS_Light = new int[] {0, 0, 1};
-            now_Light = new int[] {0, 0};
+            now_Light = new int[] {0, 0, 2};
         }
         return now_Light;
     }
@@ -136,6 +108,20 @@ public class physicalTrafficSignal {
         }
     }
 }
+
+public void countDown(int countDownSeconds){
+    for (int i = countDownSeconds; i >= 0; i--) {
+        seconds = countDownSeconds;
+        try {
+            Thread.sleep(1000); // 暫停 1 秒
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+public void getSecond(){
+    return seconds;
+}
 /*
  * 當EW_side=1 東西向通行
  * EW_g_light<=1 NS_r_light<=1 EW綠燈 NS紅燈
@@ -145,7 +131,7 @@ public class physicalTrafficSignal {
  * NS_g_light<=1 EW_r_light<=1 
  * NS_y_light<=1 EW_r_light<=1
  * NS_r_light<=1 EW_r_light<=1 全紅
-*/
+ */
 /* 紅綠燈轉換情況
  * 高、普、低、緊
  * 
