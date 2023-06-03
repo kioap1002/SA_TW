2b0b780364d2aa8da57c4edd062dca90928dd2e1
kioap1002 <b11023060@gemail.yuntech.edu.tw>
2023-06-04 01:03:04 +0800

    Revert "update"
    This reverts commit 047ee8402cebb5bd710001d67a7d5a60a6da0cd5.

--- src/main/java/com/example/data/roadSituation.java
+++ src/main/java/com/example/data/roadSituation.java
@@ -1,74 +1,51 @@
 package com.example.data;
 
-//單純用來設定和放置 east_westLane 跟 north_southLane 的
+import java.time.LocalDate;
+import java.util.Date;
+
+//Data Coupling
+//單純用來放置資料的物件
 public class roadSituation {
-    private boolean east_westLane = false; // ew有無緊急車輛，false = ew車道上無緊急車輛
-    private boolean north_southLane = false; // ns有無緊急車輛，false = ns車道上無緊急車輛
-    private double east_westDensity;
-    private double north_southDensity;
+    private Date time;
+    private LocalDate time_day;
+    private boolean laneDirection;
+    protected double vehicleAmount; // private
+    protected double vehicleAmount_aver;
+    protected boolean emergencyVehicle; // private
+    protected double emergencyVehicle_count;
+    protected double density; // private
+    protected double density_aver; // private
+    // private roadSituation_emergency RS_E;
 
-    roadSituation(boolean EV_EW, boolean EV_NS, double D_EW, double D_NS) {
-        east_westLane = EV_EW;
-        north_southLane = EV_NS;
-        east_westDensity = D_EW;
-        north_southDensity = D_NS;
+    roadSituation(Date T, boolean LD, double VA, boolean EV, double den) {
+        time = T;
+        laneDirection = LD;
+        vehicleAmount = VA;
+        emergencyVehicle = EV;
+        density = den;
     }
 
-    // 回傳緊急車輛出現狀態(有無緊急車輛, if 有: 車道方向為何)
-    public int haveEmergency() { // 0:no_EV, 1:e_w_EV, 2:n_s_EV, 3:both_EV
-        if (east_westLane && north_southLane) {
-            return 3;
-        } else if (north_southLane) {
-            return 2;
-        } else if (east_westLane) {
-            return 1;
-        } else {
-            return 0;
-        }
+    roadSituation(LocalDate T, boolean LD, double VA_aver, double EV_n, double den_aver) {
+        time_day = T;
+        laneDirection = LD;
+        vehicleAmount_aver = VA_aver;
+        emergencyVehicle_count = EV_n;
+        density_aver = den_aver;
     }
 
-    // public int densityMode_col(double Last30DaysDensity_EW, double
-    // Last30DaysDensity_NS){ //密度模板 0: 低 1:正常 2:高
-    public int densityMode_col(double Last30DaysDensity) { // 密度模板 0: 低 1:正常 2:高
-        int EW = 0, NS = 0;
-        if (east_westDensity > Last30DaysDensity * 1.5) {
-            EW = 2; // 高密度
-        } else if (east_westDensity < Last30DaysDensity * 0.5) {
-            EW = 0; // 低密度
-        } else {
-            EW = 1; // 普通密度
-        }
-        if (north_southDensity > Last30DaysDensity * 1.5) {
-            NS = 2;
-        } else if (north_southDensity < Last30DaysDensity * 0.5) {
-            NS = 0;
-        } else {
-            NS = 1;
-        }
-        return densityMode(EW, NS);
+    public boolean getLaneDirection() {
+        return laneDirection;
     }
 
-    // 單處理密度 密度模板 0: both高 1: EW高 2:NS高, 3: NS or EW普通, 4: both低
-    public int densityMode(int EW, int NS) {
-        int mode_D;
-        if (EW == 2 || NS == 2) {
-            if (EW == 2 && NS == 2) {
-                // 兩邊高密度，增加時間配比
-                mode_D = 0;
-            } else if (EW == 2) {
-                // EW高密度，增加綠燈秒數
-                mode_D = 1;
-            } else {
-                // NS高密度，增加紅燈秒數
-                mode_D = 2;
-            }
-        } else if (EW == 1 || NS == 1) {
-            // 套用正常模板
-            mode_D = 3;
-        } else {
-            // 套用低密度模板
-            mode_D = 4;
-        }
-        return mode_D;
+    public double getVehicleAmount() {
+        return vehicleAmount;
+    }
+
+    public boolean getEmergencyVehicle() {
+        return emergencyVehicle;
+    }
+
+    public double getDensity() {
+        return density;
     }
 }
