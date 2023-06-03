e8a98adc0fa32bf074cd67274f2ec5b3b716dd65
dreamer5227 <B11023005@gemail.yuntech.edu.tw>
2023-06-03 17:57:12 +0800

    相機測試

--- cameraTest.py
+++ cameraTest.py
@@ -1,27 +1,36 @@
 import cv2
 import time
+import pymysql
 class CCTV:
     def __init__(self):
-        self.video_capture = cv2.VideoCapture(0) # 打开影片流 # 0代表默认摄像头
-        self.start_time = time.time() # 设置计时器
-    # 處理照片的部分，使用AI偵測，未完成
-    def process_image(self, image):
-        # 程式碼
-        height, width, _ = image.shape
-    # 运行CCTV监控
-    def run(self):
-        while True:
-            ret, frame = self.video_capture.read() # 读取影片流的帧
-            current_time = time.time() # 获取当前时间
-            if current_time - self.start_time >= 5: # 检查是否到达拍照间隔
-                image_name = f"photo_{int(current_time)}.jpg" 
-                cv2.imwrite(image_name, frame) # 保存图片
-                self.process_image(frame) # 处理图像
-                self.start_time = current_time # 更新计时器
-            # 按下 'q' 键退出循环
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        self.video_capture.release() # 释放影片流
+        self.video_capture = cv2.VideoCapture(0) # 打開影片流 # 0代表預設攝像頭
+    def capture_image(self):
+        ret, frame = self.video_capture.read() # 讀取影片流的幀
+        return frame # 返回捕獲的圖像
+class Database:
+    def __init__(self):
+        self.connection = pymysql.connect(host='localhost', user='username', password='password', db='database') # 連接到資料庫
+    def insert_image(self, image):
+        # 將影像轉換為位元組資料
+        _, img_bytes = cv2.imencode('.jpg', image)
+        img_data = img_bytes.tobytes()
+        # 插入圖像數據到資料庫
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO images (image_data) VALUES (%s)", (img_data,))
+        self.connection.commit()
+        cursor.close()
 if __name__ == "__main__":
-    cctv = CCTV() # 创建CCTV对象并运行监控
-    cctv.run()
+    db = Database() # 建立資料庫物件
+    cctv = CCTV() # 建立CCTV物件並運行監控
+    start_time = time.time() # 設定計時器
+    while True:
+        current_time = time.time() # 抓取目前的時間
+        if current_time - start_time >= 5: # 檢查是否到達拍照間隔
+            image = cctv.capture_image() # 拍攝照片
+            db.insert_image(image) # 將影像儲存到資料庫中
+            start_time = current_time # 更新計時器
+        # 按下 'q' 鍵退出迴圈
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    db.connection.close()
+    cctv.video_capture.release()
--- detectCamera.py
+++ detectCamera.py
@@ -1,5 +1,25 @@
 import cv2
+import time
+import pymysql
 import numpy as np
+class CCTV:
+    def __init__(self):
+        self.video_capture = cv2.VideoCapture(0) # 打開影片流 # 0代表預設攝像頭
+    def capture_image(self):
+        ret, frame = self.video_capture.read() # 讀取影片流的幀
+        return frame # 返回捕獲的圖像
+class Database:
+    def __init__(self): # 連接到資料庫
+        self.connection = pymysql.connect(host='localhost', user='username', password='password', db='database')
+    def insert_image(self, image, vehicle_count, emergency_count):
+        # 將影像轉換為位元組資料
+        _, img_bytes = cv2.imencode('.jpg', image)
+        img_data = img_bytes.tobytes()
+        # 插入圖像數據到資料庫
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO (image_data, vehicle_count, emergency_count) VALUES (%s, %s, %s)", (img_data, vehicle_count, emergency_count))
+        self.connection.commit()
+        cursor.close()
 class YOLODetector:
     def __init__(self): # 載入YOLOv3模型相關檔案和權重
         self.net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
@@ -44,15 +64,28 @@ class YOLODetector:
                 objects.append((label, x, y, w, h))
         return objects
 if __name__ == "__main__":
+    db = Database() # 建立資料庫物件
     detector = YOLODetector() # 創建YOLODetector物件
-    image = cv2.imread("road_image.jpg") # 讀取圖像
-    detections = detector.detect_objects(image) # 進行目標檢測
-    # 統計車輛和緊急車輛數量
-    vehicle_count = 0
-    emergency_count = False
-    for detection in detections:
-        label, _, _, _, _ = detection
-        if label == 'car' or label == 'truck':
-            vehicle_count += 1
-        elif label == 'ambulance' or label == 'fire engine' or label == 'police car':
-            emergency_count = True
+    cctv = CCTV() # 建立CCTV物件並運行監控
+    start_time = time.time() # 設定計時器
+    while True:
+        current_time = time.time() # 抓取目前的時間
+        if current_time - start_time >= 5: # 檢查是否到達拍照間隔
+            image = cctv.capture_image() # 拍攝照片
+            detections = detector.detect_objects(image)
+            # 統計車輛和緊急車輛數量
+            vehicle_count = 0
+            emergency_count = False
+            for detection in detections:
+                label, _, _, _, _ = detection
+                if label == 'car' or label == 'truck':
+                    vehicle_count += 1
+                elif label == 'ambulance' or label == 'fire engine' or label == 'police car':
+                    emergency_count = True
+            db.insert_image(image, vehicle_count, emergency_count) # 將影像儲存到資料庫中
+            start_time = current_time # 更新計時器
+        # 按下 'q' 鍵退出迴圈
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    db.connection.close()
+    cctv.video_capture.release()
